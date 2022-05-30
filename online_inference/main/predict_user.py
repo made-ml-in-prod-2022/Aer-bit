8a2186d9e2a824bd1a74f2a184c728ec5a0f7166
Alexander Ershov <aershov2020@gmail.com>
2022-05-30 10:35:44 +0600

    Added input check

--- online_inference/main/predict_user.py
+++ online_inference/main/predict_user.py
@@ -2,13 +2,35 @@ import requests
 import click
 
 
+URL = 'http://127.0.0.1:8000/'
+
+
 def predict_user(user_id):
 
-    app_url = 'http://127.0.0.1:8000/'
-    func = 'predict/'
-    r = requests.get(''.join([app_url, func, str(user_id)]))
+    if check_input(user_id):
+        r = requests.get(''.join([URL, 'predict/', str(user_id)]))
+        if r.status_code == 200:
+            print(r.json()[0])
+        else:
+            print(r.json()['detail'])
 
-    print(r.text)
+
+def check_input(user_id):
+    r = requests.get(''.join([URL, 'get-user/', str(user_id)]))
+    if r.status_code == 200:
+        user = r.json()
+        for attr in user:
+            assert user[attr] >= 0
+        assert user['age'] <= 150
+        assert user['sex'] in [0, 1]
+        assert user['cp'] in [0, 1, 2, 3]
+        assert user['restecg'] in [0, 1, 2]
+        assert user['exang'] in [0, 1]
+        assert user['slope'] in [0, 1, 2]
+        return True
+    else:
+        print(r.json()['detail'])
+        return False
 
 
 @click.command(name='predict_user')
